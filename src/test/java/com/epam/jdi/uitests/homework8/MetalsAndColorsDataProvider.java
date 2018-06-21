88c8ff04878f9921696bfb93f1a24ff4025ae20e
Anastasiia_Dremina <anastasyadremina@rambler.ru>
2018-06-21 21:58:55 +0300

    Fixes were delivered in line with provided comments

--- src/test/java/com/epam/jdi/uitests/homework8/MetalsAndColorsDataProvider.java
+++ src/test/java/com/epam/jdi/uitests/homework8/MetalsAndColorsDataProvider.java
@@ -1,39 +1,35 @@
 package com.epam.jdi.uitests.homework8;
 
 import com.epam.jdi.uitests.entities.MetalsAndColorsParameters;
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import org.testng.annotations.DataProvider;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
 
 public class MetalsAndColorsDataProvider {
-    //      TODO static import
-//      Done
+    //  TODO static import
+    //  Done
     @DataProvider(name = "readFromJson")
     public static Object[][] getData() throws FileNotFoundException {
         JsonElement jsonData = new JsonParser().parse(new FileReader("src/test/resources/sourceData/JDI_ex8_metalsColorsDataSet.json"));
         JsonObject dataSet = jsonData.getAsJsonObject();
-        Set<String> keys = dataSet.keySet();
-
-        JsonArray arr = new JsonArray();
-        for (String k : keys) {
-            arr.add(dataSet.get(k));
-        }
 
         // TODO you can just use a different type token in according to data structure...
-        List<MetalsAndColorsParameters> testData = new Gson().fromJson(arr, new TypeToken<List<MetalsAndColorsParameters>>() {
+        // Done
+        HashMap<String, MetalsAndColorsParameters> testData = new Gson().fromJson(dataSet, new TypeToken<HashMap<String, MetalsAndColorsParameters>>() {
         }.getType());
 
         Object[][] returnValue = new Object[testData.size()][1];
 
         int index = 0;
         for (Object[] each : returnValue) {
-            each[0] = testData.get(index++);
+            each[0] = testData.get("data_" + (++index));
         }
         return returnValue;
     }
